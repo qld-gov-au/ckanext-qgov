d60b8e15782066e34d8c691dc217dd2774c32f6d
antuarc <carl.antuar@dsiti.qld.gov.au>
2017-08-03 12:23:06 +1000

    [QOL-1552] remove obsolete upload filter

--- ckanext/qgov/common/controller.py
+++ ckanext/qgov/common/controller.py
@@ -1,13 +1,9 @@
-import re
 import ckan.lib.base as base
-from ckan.lib.base import BaseController, c, render, request
-from ckan.controllers.storage import StorageController
+from ckan.lib.base import BaseController, render
 from ckan.lib.render import TemplateNotFound
 
 from logging import getLogger
 LOG = getLogger(__name__)
-
-ALLOWED_EXTENSIONS = re.compile(r'.*((\.csv)|(\.xls)|(\.txt)|(\.kmz)|(\.xlsx)|(\.pdf)|(\.shp)|(\.tab)|(\.jp2)|(\.esri)|(\.gdb)|(\.jpg)|(\.tif)|(\.tiff)|(\.jpeg)|(\.xml)|(\.kml)|(\.doc)|(\.docx)|(\.rtf))$', re.I)
 
 class QGOVController(BaseController):
 
@@ -17,10 +13,3 @@ class QGOVController(BaseController):
         except TemplateNotFound:
             LOG.warn(path + " not found")
             base.abort(404)
-
-    def upload_handle(self):
-        params = dict(request.params.items())
-        originalFilename = params.get('file').filename
-        if ALLOWED_EXTENSIONS.search(originalFilename):
-            return StorageController.upload_handle(StorageController())
-        base.abort(403, 'This file type is not supported. If possible, upload the file in another format. If you continue to have problems, email Smart Service - onlineservices@smartservice.qld.gov.au')
--- ckanext/qgov/common/plugin.py
+++ ckanext/qgov/common/plugin.py
@@ -427,7 +427,6 @@ class QGOVPlugin(SingletonPlugin):
         """ Use our custom controller, and disable some unwanted URLs
         """
         controller = 'ckanext.qgov.common.controller:QGOVController'
-        routeMap.connect('/storage/upload_handle', controller=controller, action='upload_handle')
         routeMap.connect('/article/{path:[-_a-zA-Z0-9/]+}', controller=controller, action='static_content')
 
         return routeMap
