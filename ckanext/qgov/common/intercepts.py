69b488658eb00d7229d44ef08ae418ecfda5262b
antuarc <carl.antuar@smartservice.qld.gov.au>
2019-07-25 15:23:19 +1000

    [QOL-5488] cleanup
    - standardise whitespace, indentation, function and variable names
    - clean up imports
    - remove unused code
    - add documentation comments
    - add more unit tests
    - split long lines
    - move some functions into more applicable modules

--- ckanext/qgov/common/intercepts.py
+++ ckanext/qgov/common/intercepts.py
@@ -1,19 +1,28 @@
+# encoding: utf-8
+""" Monkey-patch CKAN core functions with our own implementations.
+"""
+
+import re
+import json
 from logging import getLogger
+
+import requests
+
+from ckan.common import _, config, response
 from ckan.controllers.user import UserController
 from ckan.controllers.package import PackageController
 from ckan.controllers.storage import StorageController
+from ckan.lib.navl.dictization_functions import Missing
 import ckan.logic
 import ckan.logic.action.update
 import ckan.logic.schema as schemas
+import ckan.logic.validators as validators
 from ckan.model import Session
-from ckan.lib.base import BaseController, c, render, request, abort, h
-from ckan.common import _
+from ckan.lib.base import c, request, abort, h
+from ckan.lib.uploader import Upload, ResourceUpload
+
 from ckanext.qgov.common.authenticator import QGOVUser
-from ckan.lib.uploader import *
-import plugin
-import re
-import requests
-import json
+from ckanext.qgov.common.controller import *
 
 LOG = getLogger(__name__)
 
@@ -22,21 +31,65 @@ LOGGED_IN = UserController.logged_in
 PACKAGE_EDIT = PackageController._save_edit
 RESOURCE_EDIT = PackageController.resource_edit
 
-DEFAULT_USER_SCHEMA = schemas.default_user_schema()
-USER_NEW_FORM_SCHEMA = schemas.user_new_form_schema()
-USER_EDIT_FORM_SCHEMA = schemas.user_edit_form_schema()
-DEFAULT_UPDATE_USER_SCHEMA = schemas.default_update_user_schema()
+DEFAULT_USER_SCHEMA = schemas.default_user_schema
+USER_NEW_FORM_SCHEMA = schemas.user_new_form_schema
+USER_EDIT_FORM_SCHEMA = schemas.user_edit_form_schema
+DEFAULT_UPDATE_USER_SCHEMA = schemas.default_update_user_schema
 
 UPLOAD = Upload.upload
 RESOURCE_UPLOAD = ResourceUpload.upload
 STORAGE_DOWNLOAD = StorageController.file
 RESOURCE_DOWNLOAD = PackageController.resource_download
 
+ALLOWED_EXTENSIONS = [
+    'csv',
+    'xls',
+    'txt',
+    'kmz',
+    'xlsx',
+    'pdf',
+    'shp',
+    'tab',
+    'jp2',
+    'esri',
+    'gdb',
+    'jpg',
+    'tif',
+    'tiff',
+    'jpeg',
+    'xml',
+    'kml',
+    'doc',
+    'docx',
+    'rtf',
+    'json',
+    'accdb',
+    'geojson',
+    'geotiff',
+    'topojson',
+    'gpx',
+    'html',
+    'mtl',
+    'obj',
+    'ppt',
+    'pptx',
+    'wfs',
+    'wmts',
+    'zip'
+]
+ALLOWED_EXTENSIONS_PATTERN = re.compile(r'.*\.(' + '|'.join(ALLOWED_EXTENSIONS) + ')$', re.I)
+INVALID_UPLOAD_MESSAGE = '''This file type is not supported.
+If possible, upload the file in another format.
+If you continue to have problems, email
+One Stop Shop - oss.online@dsiti.qld.gov.au
+'''
+
 EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
-ALLOWED_EXTENSIONS = re.compile(r'.*((\.csv)|(\.xls)|(\.txt)|(\.kmz)|(\.xlsx)|(\.pdf)|(\.shp)|(\.tab)|(\.jp2)|(\.esri)|(\.gdb)|(\.jpg)|(\.tif)|(\.tiff)|(\.jpeg)|(\.xml)|(\.kml)|(\.doc)|(\.docx)|(\.rtf)|(\.json)|(\.accdb)|(\.geojson)|(\.geotiff)|(\.topojson)|(\.gpx)|(\.html)|(\.mtl)|(\.obj)|(\.ppt)|(\.pptx)|(\.wfs)|(\.wmts)|(\.zip))$', re.I)
-
 def set_intercepts():
+    """ Monkey-patch to wrap/override core functions with our own.
+    """
+    validators.user_password_validator = user_password_validator
     UserController.logged_in = logged_in
     PackageController._save_edit = save_edit
     PackageController.resource_edit = validate_resource_edit
@@ -51,56 +104,78 @@ def set_intercepts():
     StorageController.file = storage_download_with_headers
     PackageController.resource_download = resource_download_with_headers
 
-def default_user_schema():
-    user_schema = DEFAULT_USER_SCHEMA
-    if 'password' in user_schema:
-        for idx, user_schema_func in enumerate(user_schema['password']):
-            if user_schema_func.__name__ == 'user_password_validator':
-                user_schema['password'][idx] = plugin.user_password_validator
+def user_password_validator(key, data, errors, context):
+    """ Strengthen the built-in password validation to require more length and complexity.
+    """
+    password_min_length = int(config.get('password_min_length', '10'))
+    password_patterns = config.get(
+        'password_patterns',
+        r'.*[0-9].*,.*[a-z].*,.*[A-Z].*,.*[-`~!@#$%^&*()_+=|\\/ ].*'
+    ).split(',')
+
+    value = data[key]
+
+    if isinstance(value, Missing):
+        pass
+    elif not isinstance(value, basestring):
+        errors[('password',)].append(_('Passwords must be strings'))
+    elif value == '':
+        pass
+    elif not len(value) >= password_min_length:
+        errors[('password',)].append(
+            _('Your password must be {min} characters or longer'.format(min=password_min_length))
+        )
+    else:
+        for policy in password_patterns:
+            if not re.search(policy, value):
+                errors[('password',)].append(
+                    _('Must contain at least one number, lowercase letter, capital letter, and symbol')
+                )
+
+def _apply_schema_validator(user_schema, field_name, validator_name='user_password_validator',
+                            validator=user_password_validator):
+    if field_name in user_schema:
+        for idx, user_schema_func in enumerate(user_schema[field_name]):
+            if user_schema_func.__name__ == validator_name:
+                user_schema[field_name][idx] = validator
     return user_schema
 
+def default_user_schema():
+    """ Add our password validator function to the default list.
+    """
+    return _apply_schema_validator(DEFAULT_USER_SCHEMA(), 'password')
+
 def user_new_form_schema():
-    user_schema = USER_NEW_FORM_SCHEMA
-    if 'password' in user_schema:
-        for idx, user_schema_func in enumerate(user_schema['password']):
-            if user_schema_func.__name__ == 'user_password_validator':
-                user_schema['password'][idx] = plugin.user_password_validator
-
-    if 'password1' in user_schema:
-        for idx, user_schema_func in enumerate(user_schema['password1']):
-            if user_schema_func.__name__ == 'user_password_validator':
-                user_schema['password1'][idx] = plugin.user_password_validator
-
+    """ Apply our password validator function when creating a new user.
+    """
+    user_schema = USER_NEW_FORM_SCHEMA()
+    user_schema = _apply_schema_validator(user_schema, 'password')
+    user_schema = _apply_schema_validator(user_schema, 'password1')
     return user_schema
 
 def user_edit_form_schema():
-    user_schema = USER_EDIT_FORM_SCHEMA
-    if 'password' in user_schema:
-        for idx, user_schema_func in enumerate(user_schema['password']):
-            if user_schema_func.__name__ == 'user_password_validator':
-                user_schema['password'][idx] = plugin.user_password_validator
-    if 'password1' in user_schema:
-        for idx, user_schema_func in enumerate(user_schema['password1']):
-            if user_schema_func.__name__ == 'user_password_validator':
-                user_schema['password1'][idx] = plugin.user_password_validator
+    """ Apply our password validator function when editing an existing user.
+    """
+    user_schema = USER_EDIT_FORM_SCHEMA()
+    user_schema = _apply_schema_validator(user_schema, 'password')
+    user_schema = _apply_schema_validator(user_schema, 'password1')
     return user_schema
 
 def default_update_user_schema():
-    user_schema = DEFAULT_UPDATE_USER_SCHEMA
-    if 'password' in user_schema:
-        for idx, user_schema_func in enumerate(user_schema['password']):
-            if user_schema_func.__name__ == 'user_password_validator':
-                user_schema['password'][idx] = plugin.user_password_validator
-    return user_schema
+    """ Apply our password validator function when updating a user.
+    """
+    return _apply_schema_validator(DEFAULT_UPDATE_USER_SCHEMA(), 'password')
 
-def unlock_account(id):
-    qgovUser = Session.query(QGOVUser).filter(QGOVUser.id == id).first()
-    if qgovUser:
-        LOG.debug("Clearing failed login attempts for {}".format(id))
-        qgovUser.login_attempts = 0
+def _unlock_account(account_id):
+    """ Unlock an account (erase the failed login attempts).
+    """
+    qgov_user = Session.query(QGOVUser).filter(QGOVUser.id == account_id).first()
+    if qgov_user:
+        LOG.debug("Clearing failed login attempts for %s", account_id)
+        qgov_user.login_attempts = 0
         Session.commit()
     else:
-        LOG.debug("Account {} not found".format(id))
+        LOG.debug("Account %s not found", account_id)
 
 def user_update(context, data_dict):
     '''
@@ -108,17 +183,19 @@ def user_update(context, data_dict):
     '''
     return_value = USER_UPDATE(context, data_dict)
     if u'reset_key' in data_dict:
-        id = ckan.logic.get_or_bust(data_dict, 'id')
-        unlock_account(id)
+        account_id = ckan.logic.get_or_bust(data_dict, 'id')
+        _unlock_account(account_id)
     return return_value
 
 def logged_in(self):
+    """ Provide a custom error code when login fails due to account lockout.
+    """
     if not c.user:
         # a number of failed login attempts greater than 10
         # indicates that the locked user is associated with the current request
-        qgovUser = Session.query(QGOVUser).filter(QGOVUser.login_attempts > 10).first()
-        if qgovUser:
-            qgovUser.login_attempts = 10
+        qgov_user = Session.query(QGOVUser).filter(QGOVUser.login_attempts > 10).first()
+        if qgov_user:
+            qgov_user.login_attempts = 10
             Session.commit()
             return self.login('account-locked')
     return LOGGED_IN(self)
@@ -126,7 +203,7 @@ def logged_in(self):
 def save_edit(self, name_or_id, context, package_type=None):
     '''
     Intercept save_edit
-    Replace author,maintainer,maintainer_email
+    Replace author, maintainer, maintainer_email
     '''
     try:
         author_email = request.POST.getone('author_email')
@@ -142,59 +219,67 @@ def save_edit(self, name_or_id, context, package_type=None):
     if request.POST.has_key('maintainer_email'):
         request.POST.__delitem__('maintainer_email')
 
-    request.POST.add('author',author_email)
-    request.POST.add('maintainer',author_email)
-    request.POST.add('maintainer_email',author_email)
+    request.POST.add('author', author_email)
+    request.POST.add('maintainer', author_email)
+    request.POST.add('maintainer_email', author_email)
 
     return PACKAGE_EDIT(self, name_or_id, context, package_type=None)
 
-def validate_resource_edit(self, id, resource_id, data=None, errors=None, error_summary=None):
+def validate_resource_edit(self, package_id, resource_id,
+                           data=None, errors=None, error_summary=None):
     '''
     Intercept save_edit
-    Replace author,maintainer,maintainer_email
+    Replace author, maintainer, maintainer_email
     '''
     if request.POST.has_key('validation_schema') and request.POST.has_key('format'):
         resource_format = request.POST.getone('format')
         validation_schema = request.POST.getone('validation_schema')
         if resource_format == 'CSV' and validation_schema and validation_schema != '':
-            schema_url = plugin.generate_download_url(id,validation_schema)
-            data_url = plugin.generate_download_url(id,resource_id)
-            validation_url = "http://goodtables.okfnlabs.org/api/run?format=csv&schema={0}&data={1}&row_limit=100000&report_limit=1000&report_type=grouped".format(schema_url,data_url)
-            r = requests.get(validation_url,verify=False)
-            if r.status_code == requests.codes.ok:
-                response_text = json.loads(r.text)
-                if response_text['success'] == True:
+            schema_url = plugin.generate_download_url(package_id, validation_schema)
+            data_url = plugin.generate_download_url(package_id, resource_id)
+            validation_url = "http://goodtables.okfnlabs.org/api/run?format=csv&schema={0}&data={1}&row_limit=100000&report_limit=1000&report_type=grouped".format(schema_url, data_url)
+            req = requests.get(validation_url, verify=False)
+            if req.status_code == requests.codes.ok:
+                response_text = json.loads(req.text)
+                if response_text['success']:
                     h.flash_success("CSV was validated successfully against the selected schema")
                 else:
                     h.flash_error("CSV was NOT validated against the selected schema")
 
-    return RESOURCE_EDIT(self, id, resource_id, data, errors, error_summary)
+    return RESOURCE_EDIT(self, package_id, resource_id, data, errors, error_summary)
 
-def upload_after_validation(self, max_size = 2):
-    if self.upload_field_storage and self.upload_field_storage.filename and not ALLOWED_EXTENSIONS.search(self.upload_field_storage.filename):
+def upload_after_validation(self, max_size=2):
+    """ Validate file type against our whitelist before uploading.
+    """
+    if self.upload_field_storage and self.upload_field_storage.filename and not ALLOWED_EXTENSIONS_PATTERN.search(self.upload_field_storage.filename):
         raise ckan.logic.ValidationError(
-            {self.file_field: ['This file type is not supported. If possible, upload the file in another format. If you continue to have problems, email One Stop Shop - oss.online@dsiti.qld.gov.au']}
+            {self.file_field: [INVALID_UPLOAD_MESSAGE]}
         )
     UPLOAD(self, max_size)
 
-def resource_upload_after_validation(self, id, max_size = 2):
-    if self.filename and not ALLOWED_EXTENSIONS.search(self.filename):
+def resource_upload_after_validation(self, resource_id, max_size=2):
+    """ Validate file type against our whitelist before uploading.
+    """
+    if self.filename and not ALLOWED_EXTENSIONS_PATTERN.search(self.filename):
         raise ckan.logic.ValidationError(
-            {'upload': ['This file type is not supported. If possible, upload the file in another format. If you continue to have problems, email One Stop Shop - oss.online@dsiti.qld.gov.au']}
+            {'upload': [INVALID_UPLOAD_MESSAGE]}
         )
-    RESOURCE_UPLOAD(self, id, max_size)
+    RESOURCE_UPLOAD(self, resource_id, max_size)
 
-def set_download_headers():
-    from ckan.common import response
+def _set_download_headers(response):
     response.headers['Content-Disposition'] = 'attachment'
     response.headers['X-Content-Type-Options'] = 'nosniff'
 
 def storage_download_with_headers(self, label):
+    """ Add security headers to protect against download-based exploits.
+    """
     file_download = STORAGE_DOWNLOAD(self, label)
-    set_download_headers()
+    _set_download_headers(response)
     return file_download
 
-def resource_download_with_headers(self, id, resource_id, filename = None):
-    file_download = RESOURCE_DOWNLOAD(self, id, resource_id, filename)
-    set_download_headers()
+def resource_download_with_headers(self, package_id, resource_id, filename=None):
+    """ Add security headers to protect against download-based exploits.
+    """
+    file_download = RESOURCE_DOWNLOAD(self, package_id, resource_id, filename)
+    _set_download_headers(response)
     return file_download
--- ckanext/qgov/common/urlm.py
+++ ckanext/qgov/common/urlm.py
@@ -1,14 +1,19 @@
+# encoding: utf-8
+""" Functions for URL Management System interaction.
+"""
+from logging import getLogger
+
 import ckan.lib.base as base
 import ckan.controllers.group as group
 import ckan.controllers.package as package
 import ckan.controllers.user as user
-
 import ckan.lib.helpers as helpers
 
-from logging import getLogger
 LOG = getLogger(__name__)
 
 RAW_ABORT = base.abort
+URLM_ENDPOINT = None
+URLM_PROXY = None
 
 def configure_urlm(app_path, proxy):
     """
@@ -19,9 +24,11 @@ def configure_urlm(app_path, proxy):
     URLM_ENDPOINT = app_path
     URLM_PROXY = proxy
 
-    LOG.info("Using URL Management system at {endpoint} via proxy {proxy}".format(endpoint=URLM_ENDPOINT, proxy=URLM_PROXY))
+    LOG.info("Using URL Management system at %s via proxy %s", URLM_ENDPOINT, URLM_PROXY)
 
 def intercept_404():
+    """ Monkey-patch ourselves into the 404 handler.
+    """
     base.abort = abort_with_purl
     group.abort = base.abort
     package.abort = base.abort
@@ -29,26 +36,32 @@ def intercept_404():
     user.abort = base.abort
 
 def abort_with_purl(status_code=None, detail='', headers=None, comment=None):
+    """ Consult PURL about a 404, redirecting if it reports a new URL.
+    """
     if status_code == 404:
         global URLM_ENDPOINT, URLM_PROXY
-        LOG.warn("Page [{path}] not found; checking URL Management system at {endpoint}".format(path=base.request.url, endpoint=URLM_ENDPOINT))
-        purl_request = URLM_ENDPOINT.format(source = base.request.url)
+        LOG.warn("Page [%s] not found; checking URL Management System at %s",
+                 base.request.url, URLM_ENDPOINT)
+        purl_request = URLM_ENDPOINT.format(source=base.request.url)
         try:
-            import json, urllib2
-            if URLM_PROXY == None:
-                r = urllib2.urlopen(purl_request)
+            import json
+            import urllib2
+            if URLM_PROXY:
+                proxy_handler = urllib2.ProxyHandler(
+                    {'http': 'http://' + URLM_PROXY, 'https': 'https://' + URLM_PROXY}
+                )
+                req = urllib2.build_opener(proxy_handler).open(purl_request)
             else:
-                r = urllib2.build_opener(urllib2.ProxyHandler({'http': 'http://' + URLM_PROXY, 'https': 'https://' + URLM_PROXY})).open(purl_request)
-            response = json.load(r)
+                req = urllib2.urlopen(purl_request)
+            response = json.load(req)
             if response['Status'] == 301:
                 location = response['Headers']['location']
-                LOG.info("Found; redirecting to {location}".format(location=location))
+                LOG.info("Found; redirecting to %s", location)
                 helpers.redirect_to(location, 301)
                 return
             else:
-                LOG.warn("No match in URL Management system")
-        except urllib2.URLError as e:
-            LOG.error("Failed to contact URL Management system: {error}".format(error=e))
-            pass
-    return RAW_ABORT(status_code, detail, headers, comment)
+                LOG.warn("No match in URL Management System")
+        except urllib2.URLError as ex:
+            LOG.error("Failed to contact URL Management system: %s", ex)
 
+    return RAW_ABORT(status_code, detail, headers, comment)
