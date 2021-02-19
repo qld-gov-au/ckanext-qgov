036dd1ca6d7a787acb5b054038733a4151c677f9
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-02-19 11:04:11 +1000

    [QOL-7762][QOL-1552] add file samples for testing MIME type validation

--- ckanext/qgov/common/test_mime_type_validation.py
+++ ckanext/qgov/common/test_mime_type_validation.py
@@ -7,6 +7,7 @@ import unittest
 
 import intercepts
 import ckan.logic
+from werkzeug.datastructures import FileStorage as FlaskFileStorage
 
 
 text_type = 'text/csv'
@@ -52,6 +53,56 @@ class TestMimeTypeValidation(unittest.TestCase):
         with a different prefix.
         """
         self.assertRaises(ckan.logic.ValidationError, intercepts.coalesce_mime_types, ['text/plain', application_type])
+
+    # Full validation tests
+
+    def test_validate_upload_filename_and_format(self):
+        """ Test that uploaded resources have their file extension and
+        format compared to each other.
+        """
+        intercepts.configure({})
+        upload = FlaskFileStorage(filename="dummy.pdf", stream=open("test/resources/dummy.pdf"))
+        resource = {'url': 'example.csv', 'format': 'PDF', 'upload': upload}
+        self.assertRaises(ckan.logic.ValidationError, intercepts.validate_resource_mimetype, resource)
+        self.assertIsNone(resource.get('mimetype'))
+
+        resource['url'] = 'example.pdf'
+        resource['format'] = 'XML'
+        self.assertRaises(ckan.logic.ValidationError, intercepts.validate_resource_mimetype, resource)
+        self.assertIsNone(resource.get('mimetype'))
+
+        resource['format'] = 'PDF'
+        intercepts.validate_resource_mimetype(resource)
+        self.assertEqual(resource['mimetype'], 'application/pdf')
+
+        upload = FlaskFileStorage(filename="example.csv", stream=open("test/resources/foo.csv"))
+        resource = {'url': 'example.csv', 'format': 'CSV', 'upload': upload}
+        intercepts.validate_resource_mimetype(resource)
+        self.assertEqual(resource['mimetype'], 'text/csv')
+
+    def test_validate_upload_content(self):
+        """ Test that uploaded resources have their contents compared
+        to their claimed file format and extension.
+        """
+        upload = FlaskFileStorage(filename="eicar.com.pdf", stream=open("test/resources/eicar.com.pdf"))
+        resource = {'url': 'example.pdf', 'format': 'PDF', 'upload': upload}
+        self.assertRaises(ckan.logic.ValidationError, intercepts.validate_resource_mimetype, resource)
+
+        resource['url'] = 'example.txt'
+        resource['format'] = 'TXT'
+        intercepts.validate_resource_mimetype(resource)
+        self.assertEqual(resource['mimetype'], 'text/plain')
+
+    def test_revalidate_uploads_without_file(self):
+        """ Test that resource of type 'upload' with no upload data
+        have their URL and format compared, just no content sniffing.
+        """
+        intercepts.configure({})
+        resource = {'url': 'example.csv', 'format': 'PDF'}
+        self.assertRaises(ckan.logic.ValidationError, intercepts.validate_resource_mimetype, resource)
+
+        resource['format'] = 'CSV'
+        intercepts.validate_resource_mimetype(resource)
 
 
 if __name__ == '__main__':
