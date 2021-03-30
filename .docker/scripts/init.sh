07cd5608b4a5137b61f74b7d279e5470fe26a0ba
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-03-30 16:17:27 +1000

    [QOL-7890] prepare for CKAN 2.9
    - adjust CLI calls to handle either 'paster' or 'ckan' depending on what is present

--- .docker/scripts/init.sh
+++ .docker/scripts/init.sh
@@ -8,19 +8,24 @@ CKAN_USER_NAME="${CKAN_USER_NAME:-admin}"
 CKAN_DISPLAY_NAME="${CKAN_DISPLAY_NAME:-Administrator}"
 CKAN_USER_PASSWORD="${CKAN_USER_PASSWORD:-Password123!}"
 CKAN_USER_EMAIL="${CKAN_USER_EMAIL:-admin@localhost}"
+CKAN_INI_FILE=/app/ckan/default/production.ini
 
 . /app/ckan/default/bin/activate
-which ckan || ckan () {
-    paster --plugin=ckan $* -c /app/ckan/default/production.ini
+
+ckan_cli () {
+    if (which ckan > /dev/null); then
+        ckan -c ${CKAN_INI_FILE} $*
+    else
+        paster --plugin=ckan $* -c ${CKAN_INI_FILE}
 }
 cd /app/ckan/default/src/ckan
-ckan db clean || exit 1
-ckan db init || exit 1
-ckan user add "${CKAN_USER_NAME}"\
+ckan_cli db clean || exit 1
+ckan_cli db init || exit 1
+ckan_cli user add "${CKAN_USER_NAME}"\
  fullname="${CKAN_DISPLAY_NAME}"\
  email="${CKAN_USER_EMAIL}"\
  password="${CKAN_USER_PASSWORD}" || exit 1
-ckan sysadmin add "${CKAN_USER_NAME}" || exit 1
+ckan_cli sysadmin add "${CKAN_USER_NAME}" || exit 1
 
 # Create some base test data
 . /app/scripts/create-test-data.sh
