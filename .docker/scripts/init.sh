85e09f5cc6a6243fec648baaf9e2921b0df1bd04
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-03-30 15:30:21 +1000

    [QOL-7890] include display name in initial test data
    - also fail-fast if administrator cannot be created

--- .docker/scripts/init.sh
+++ .docker/scripts/init.sh
@@ -5,15 +5,20 @@
 set -e
 
 CKAN_USER_NAME="${CKAN_USER_NAME:-admin}"
+CKAN_DISPLAY_NAME="${CKAN_DISPLAY_NAME:-Administrator}"
 CKAN_USER_PASSWORD="${CKAN_USER_PASSWORD:-Password123!}"
 CKAN_USER_EMAIL="${CKAN_USER_EMAIL:-admin@localhost}"
 
-. /app/ckan/default/bin/activate \
-  && cd /app/ckan/default/src/ckan \
-  && paster db clean -c /app/ckan/default/production.ini \
-  && paster db init -c /app/ckan/default/production.ini \
-  && paster --plugin=ckan user add "${CKAN_USER_NAME}" email="${CKAN_USER_EMAIL}" password="${CKAN_USER_PASSWORD}" -c /app/ckan/default/production.ini \
-  && paster --plugin=ckan sysadmin add "${CKAN_USER_NAME}" -c /app/ckan/default/production.ini
+. /app/ckan/default/bin/activate
+which ckan || (function ckan { paster --plugin=ckan $* -c /app/ckan/default/production.ini || exit 1 })
+cd /app/ckan/default/src/ckan
+ckan db clean || exit 1
+ckan db init || exit 1
+ckan user add "${CKAN_USER_NAME}"\
+ fullname="${CKAN_DISPLAY_NAME}"\
+ email="${CKAN_USER_EMAIL}"\
+ password="${CKAN_USER_PASSWORD}" || exit 1
+ckan sysadmin add "${CKAN_USER_NAME}" || exit 1
 
 # Create some base test data
 . /app/scripts/create-test-data.sh
