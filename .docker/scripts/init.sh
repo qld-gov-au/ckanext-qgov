2bacfc614f86379fa50773dfb673899ce093d569
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-06-10 09:29:20 +1000

    [QOL-8055] oops fix path to docker scripts

--- .docker/scripts/init.sh
+++ .docker/scripts/init.sh
@@ -10,14 +10,13 @@ CKAN_USER_PASSWORD="${CKAN_USER_PASSWORD:-Password123!}"
 CKAN_USER_EMAIL="${CKAN_USER_EMAIL:-admin@localhost}"
 
 . ${APP_DIR}/bin/activate
-CKAN_CLI=$WORKDIR/scripts/ckan_cli
-$CKAN_CLI db clean
-$CKAN_CLI db init
-$CKAN_CLI user add "${CKAN_USER_NAME}"\
+ckan_cli db clean
+ckan_cli db init
+ckan_cli user add "${CKAN_USER_NAME}"\
  fullname="${CKAN_DISPLAY_NAME}"\
  email="${CKAN_USER_EMAIL}"\
  password="${CKAN_USER_PASSWORD}"
-$CKAN_CLI sysadmin add "${CKAN_USER_NAME}"
+ckan_cli sysadmin add "${CKAN_USER_NAME}"
 
 # Create some base test data
 . $WORKDIR/scripts/create-test-data.sh
