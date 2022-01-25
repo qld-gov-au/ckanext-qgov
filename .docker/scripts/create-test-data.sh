2c22f25946a61c54cec891b6c865022ce9e2a179
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-01-25 11:58:41 +1000

    [QOL-8595] add test org and members

--- .docker/scripts/create-test-data.sh
+++ .docker/scripts/create-test-data.sh
@@ -32,6 +32,46 @@ if [ "$API_KEY" = "None" ]; then
     API_KEY=$(ckan_cli user token add "${CKAN_USER_NAME}" test_setup |grep -v '^API Token created' | tr -d '[:space:]')
 fi
 
+##
+# BEGIN: Create a test organisation with test users for admin, editor and member
+#
+TEST_ORG_NAME=test-organisation
+TEST_ORG_TITLE="Test Organisation"
+
+echo "Creating test users for ${TEST_ORG_TITLE} Organisation:"
+
+add_user_if_needed ckan_user "CKAN User" ckan_user@localhost
+add_user_if_needed test_org_admin "Test Admin" test_org_admin@localhost
+add_user_if_needed test_org_editor "Test Editor" test_org_editor@localhost
+add_user_if_needed test_org_member "Test Member" test_org_member@localhost
+
+echo "Creating ${TEST_ORG_TITLE} Organisation:"
+
+TEST_ORG=$( \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "name=${TEST_ORG_NAME}&title=${TEST_ORG_TITLE}" \
+    ${CKAN_ACTION_URL}/organization_create
+)
+
+TEST_ORG_ID=$(echo $TEST_ORG | sed -r 's/^(.*)"id": "(.*)",(.*)/\2/')
+
+echo "Assigning test users to ${TEST_ORG_TITLE} Organisation:"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${TEST_ORG_ID}&object=test_org_admin&object_type=user&capacity=admin" \
+    ${CKAN_ACTION_URL}/member_create
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${TEST_ORG_ID}&object=test_org_editor&object_type=user&capacity=editor" \
+    ${CKAN_ACTION_URL}/member_create
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${TEST_ORG_ID}&object=test_org_member&object_type=user&capacity=member" \
+    ${CKAN_ACTION_URL}/member_create
+##
+# END.
+#
+
 # Creating test data hierarchy which creates organisations assigned to datasets
 ckan_cli create-test-data hierarchy
 
@@ -44,10 +84,20 @@ add_user_if_needed foodie "Foodie" foodie@localhost
 add_user_if_needed group_admin "Group Admin" group_admin@localhost
 add_user_if_needed walker "Walker" walker@localhost
 
-echo "Updating annakarenina to use department-of-health Organisation:"
+echo "Assigning test Datasets to Organisation..."
+
+echo "Updating annakarenina to use ${TEST_ORG_TITLE} organisation:"
 package_owner_org_update=$( \
     curl -LsH "Authorization: ${API_KEY}" \
-    --data "id=annakarenina&organization_id=department-of-health" \
+    --data "id=annakarenina&organization_id=${TEST_ORG_NAME}" \
+    ${CKAN_ACTION_URL}/package_owner_org_update
+)
+echo ${package_owner_org_update}
+
+echo "Updating warandpeace to use ${TEST_ORG_TITLE} organisation:"
+package_owner_org_update=$( \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=warandpeace&organization_id=${TEST_ORG_NAME}" \
     ${CKAN_ACTION_URL}/package_owner_org_update
 )
 echo ${package_owner_org_update}
