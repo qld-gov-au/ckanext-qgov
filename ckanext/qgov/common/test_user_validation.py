328c62cfc1dcd018de226da659c398428d492a32
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-02-03 10:49:30 +1000

    [QOL-8595] fix bugs in 'displayed name' validation
    - skip validation if resetting password
    - permit update if displayed name is unchanged

--- ckanext/qgov/common/test_user_validation.py
+++ ckanext/qgov/common/test_user_validation.py
@@ -3,6 +3,7 @@
 '''Tests for username and Displayed Name filtering.
 '''
 
+import mock
 import unittest
 
 from ckan.plugins import toolkit
@@ -10,7 +11,9 @@ from ckan.plugins import toolkit
 from .user_creation import validators
 
 PUBLISHER_USERNAME = 'publisher-123'
+PUBLISHER_DISPLAY_NAME = 'Gov account'
 NON_PUBLISHER_USERNAME = 'test-123'
+NON_PUBLISHER_DISPLAY_NAME = 'Public account'
 
 
 class MockUser(object):
@@ -27,42 +30,59 @@ class MockUser(object):
 
 def mock_objects(username='', displayed_name='', sysadmin=False):
     validators._get_user = lambda: MockUser(sysadmin, username, displayed_name)
+    validators.config = {'ckanext.data_qld.excluded_display_name_words': 'gov'}
 
 
 class TestUserValidation(unittest.TestCase):
     """ Test our user validation rules.
     """
 
+    def _assert_valid(self, username, fullname, context=None):
+        data_dict = {'username': username, 'fullname': fullname}
+        validators.data_qld_user_name_validator(key='username', data=data_dict, errors=None, context=context)
+        validators.data_qld_displayed_name_validator(key='fullname', data=data_dict, errors=None, context=context)
+
+    def _assert_not_valid_username(self, username):
+        self.assertRaises(
+            toolkit.Invalid, validators.data_qld_user_name_validator,
+            'username', {'username': username, 'fullname': NON_PUBLISHER_DISPLAY_NAME}, None, None)
+
+    def _assert_not_valid_display_name(self, fullname):
+        self.assertRaises(
+            toolkit.Invalid, validators.data_qld_displayed_name_validator,
+            'fullname', {'username': NON_PUBLISHER_USERNAME, 'fullname': fullname}, None, None)
+
     def test_can_register_user(self):
         """ Test that a user can be created/updated.
         """
         mock_objects()
-        validators.data_qld_user_name_validator('username', {'username': NON_PUBLISHER_USERNAME}, None, None)
+        self._assert_valid(NON_PUBLISHER_USERNAME, NON_PUBLISHER_DISPLAY_NAME)
 
     def test_cannot_set_publisher_name(self):
         """ Test that usernames may not contain 'publisher' by default.
         """
         mock_objects()
-        self.assertRaises(toolkit.Invalid, validators.data_qld_user_name_validator, 'username', {'username': PUBLISHER_USERNAME}, None, None)
+        self._assert_not_valid_username(PUBLISHER_USERNAME)
+        self._assert_not_valid_display_name(PUBLISHER_DISPLAY_NAME)
 
     def test_sysadmin_can_set_publisher_name(self):
         """ Test that sysadmins can update usernames to contain 'publisher'.
         """
         mock_objects(sysadmin=True)
-        validators.data_qld_user_name_validator('username', {'username': PUBLISHER_USERNAME}, None, None)
+        self._assert_valid(PUBLISHER_USERNAME, PUBLISHER_DISPLAY_NAME)
 
     def test_publisher_can_retain_name(self):
         """ Test that publishers can update their profiles
         without changing their usernames.
         """
-        mock_objects(username=PUBLISHER_USERNAME)
-        validators.data_qld_user_name_validator('username', {'username': PUBLISHER_USERNAME}, None, None)
+        mock_objects(username=PUBLISHER_USERNAME, displayed_name=PUBLISHER_DISPLAY_NAME)
+        self._assert_valid(PUBLISHER_USERNAME, PUBLISHER_DISPLAY_NAME)
 
     def test_publisher_can_reset_password(self):
         """ Test that publishers can reset their passwords.
         """
         mock_objects()
-        validators.data_qld_user_name_validator('username', {'username': PUBLISHER_USERNAME}, None, {'reset_password': True})
+        self._assert_valid(PUBLISHER_USERNAME, PUBLISHER_DISPLAY_NAME, context={'reset_password': True})
 
 
 if __name__ == '__main__':
--- ckanext/qgov/common/user_creation/validators.py
+++ ckanext/qgov/common/user_creation/validators.py
@@ -1,12 +1,14 @@
-from ckan.plugins import toolkit
+# encoding: utf-8
+
+from ckan.plugins.toolkit import config, g, Invalid
 
 
 def _get_user():
     """ Retrieve the current user object.
     """
     # 'g' is not a regular data structure so we can't use 'hasattr'
-    if 'userobj' in dir(toolkit.g):
-        return toolkit.g.userobj
+    if 'userobj' in dir(g):
+        return g.userobj
     else:
         return None
 
@@ -24,17 +26,25 @@ def data_qld_user_name_validator(key, data, errors, context):
     new_username = data[key].lower()
 
     if not is_sysadmin and 'publisher' in new_username and old_username != new_username:
-        raise toolkit.Invalid("The username cannot contain the word 'publisher'. Please enter another username.")
+        raise Invalid("The username cannot contain the word 'publisher'. Please enter another username.")
 
 
 def data_qld_displayed_name_validator(key, data, errors, context):
+    if context and context.get('reset_password', False):
+        return
     user = _get_user()
-    is_sysadmin = user is not None and user.sysadmin
+    if user is None:
+        is_sysadmin = False
+        old_name = None
+    else:
+        is_sysadmin = user.sysadmin
+        old_name = user.fullname.lower()
+    new_name = data[key].lower()
 
-    if not is_sysadmin:
-        excluded_names = toolkit.config.get('ckanext.data_qld.excluded_display_name_words', '').split('\r\n')
+    if not is_sysadmin and old_name != new_name:
+        excluded_names = config.get('ckanext.data_qld.excluded_display_name_words', '').split('\r\n')
         for name in excluded_names:
             # In some case, name value can be "   ", we need to remove the space.
-            if name.strip() and name.strip().lower() in data[key].lower():
-                raise toolkit.Invalid(
+            if name.strip() and name.strip().lower() in new_name:
+                raise Invalid(
                     "The displayed name cannot contain certain words such as 'publisher', 'QLD Government' or similar. Please enter another display name.")
