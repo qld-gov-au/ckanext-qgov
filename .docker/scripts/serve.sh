4d96f4b1d06e7eb4f9dab4baeaf218e7b3241a42
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-08-16 09:59:46 +1000

    [QOL-9199] ensure PostgreSQL is accepting connections before starting CKAN
    - Just listening on the port is not sufficient. Could use pg_isready if we were on the database server

--- .docker/scripts/serve.sh
+++ .docker/scripts/serve.sh
@@ -5,6 +5,14 @@ dockerize -wait tcp://postgres:5432 -timeout 1m
 dockerize -wait tcp://solr:8983 -timeout 1m
 dockerize -wait tcp://redis:6379 -timeout 1m
 
+for i in {1..60}; do
+    if (PGPASSWORD=pass psql -h postgres -U ckan_default -d ckan_test -c "\q"); then
+        break
+    else
+        sleep 1
+    fi
+done
+
 . ${APP_DIR}/scripts/activate
 if (which ckan > /dev/null); then
     ckan -c ${CKAN_INI} run -r
