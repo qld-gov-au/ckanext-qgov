983f1f1a55ad88cf93af85bd5f98690041c8b65b
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-10-13 13:35:22 +1000

    [QOL-9410] fix test database health check

--- .docker/scripts/serve.sh
+++ .docker/scripts/serve.sh
@@ -5,7 +5,7 @@ dockerize -wait tcp://postgres:5432 -timeout 1m
 dockerize -wait tcp://solr:8983 -timeout 1m
 dockerize -wait tcp://redis:6379 -timeout 1m
 
-for i in {1..60}; do
+for i in `seq 1 60`; do
     if (PGPASSWORD=pass psql -h postgres -U ckan_default -d ckan_test -c "\q"); then
         break
     else
