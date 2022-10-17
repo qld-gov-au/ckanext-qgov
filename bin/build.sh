c49082c9fd012fb5d668a31850da75dbb36ca091
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-10-17 14:10:23 +1000

    [QOL-9410] use custom CKAN fork

--- bin/build.sh
+++ bin/build.sh
@@ -15,14 +15,21 @@ sed -i -e "s/##//" docker-compose.yml
 ahoy pull
 
 PYTHON=python
-if [ "$CKAN_VERSION" = "2.8" ] || [ "$CKAN_VERSION" = "2.9-py2" ]; then
+if [ "$CKAN_VERSION" = "2.8" ]; then
     PYTHON_VERSION=py2
+    QGOV_CKAN_VERSION=ckan-2.8.8-qgov.5
 else
-    PYTHON_VERSION=py3
-    PYTHON="${PYTHON}3"
+    QGOV_CKAN_VERSION=ckan-2.9.5-qgov.8
+    if [ "$CKAN_VERSION" = "2.9-py2" ]; then
+        PYTHON_VERSION=py2
+    else
+        PYTHON_VERSION=py3
+        PYTHON="${PYTHON}3"
+    fi
 fi
 
 sed "s|{CKAN_VERSION}|$CKAN_VERSION|g" .docker/Dockerfile-template.ckan \
+    | sed "s|{QGOV_CKAN_VERSION}|$QGOV_CKAN_VERSION|g" \
     | sed "s|{PYTHON_VERSION}|$PYTHON_VERSION|g" \
     | sed "s|{PYTHON}|$PYTHON|g" \
     > .docker/Dockerfile.ckan
