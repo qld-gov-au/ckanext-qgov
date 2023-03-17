465d31652218c242033e5668c7be3ee60637b9a7
antuarc <carl.antuar@smartservice.qld.gov.au>
2023-03-17 12:30:38 +1000

    [QOLDEV-313] enable the 'activity' plugin on CKAN 2.10
    - Activity streams have now moved to this plugin

--- bin/init-ext.sh
+++ bin/init-ext.sh
@@ -3,6 +3,7 @@
 # Install current extension.
 #
 set -e
+set -x
 
 install_requirements () {
     PROJECT_DIR=$1
@@ -37,4 +38,7 @@ installed_name=$(grep '^\s*name=' setup.py |sed "s|[^']*'\([-a-zA-Z0-9]*\)'.*|\1
 # Validate that the extension was installed correctly.
 if ! pip list | grep "$installed_name" > /dev/null; then echo "Unable to find the extension in the list"; exit 1; fi
 
+if [ -d "$SRC_DIR/ckan/ckanext/activity" ]; then
+    sed -i 's|^ckan.plugins = |ckan.plugins = activity |' $CKAN_INI
+fi
 . ${APP_DIR}/bin/deactivate
