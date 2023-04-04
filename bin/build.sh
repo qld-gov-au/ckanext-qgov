08051cc46aa62a091d5bf044223056fe8832d9e7
ThrawnCA <shell_layer-github@yahoo.com.au>
2023-04-04 16:56:11 +1000

    [QOLDEV-313] remove CKAN 2.8 rules from scenario tests

--- bin/build.sh
+++ bin/build.sh
@@ -19,13 +19,7 @@ PYTHON=python
 CKAN_GIT_VERSION=$CKAN_VERSION
 CKAN_GIT_ORG=ckan
 
-if [ "$CKAN_VERSION" = "2.8" ]; then
-    PYTHON_VERSION=py2
-    if [ "$CKAN_TYPE" = "custom" ]; then
-      CKAN_GIT_VERSION=ckan-2.8.8-qgov.5
-      CKAN_GIT_ORG=qld-gov-au
-    fi
-elif [ "$CKAN_VERSION" = "2.10" ]; then
+if [ "$CKAN_VERSION" = "2.10" ]; then
     PYTHON_VERSION=py3
     PYTHON="${PYTHON}3"
 else
