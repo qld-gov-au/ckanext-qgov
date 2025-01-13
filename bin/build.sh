eb80e967fa482cb3713de70556c9346b9cc1bce4
ThrawnCA <shell_layer-github@yahoo.com.au>
2025-01-13 11:06:39 +1000

    [QOLDEV-955] update test CKAN fork to provide CAPTCHA error message on login

--- bin/build.sh
+++ bin/build.sh
@@ -28,7 +28,7 @@ if [ "$CKAN_VERSION" = "2.11" ]; then
     CKAN_GIT_VERSION=ckan-2.11.1
 elif [ "$CKAN_VERSION" = "2.10" ]; then
     if [ "$CKAN_TYPE" = "custom" ]; then
-        CKAN_GIT_VERSION=ckan-2.10.5-qgov.4
+        CKAN_GIT_VERSION=ckan-2.10.5-qgov.5
     else
         CKAN_GIT_VERSION=ckan-2.10.5
     fi
