ff6a28b18445bb8b679a2aa780cac24c39cd5de2
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2025-04-08 12:06:16 +1000

    [QOLSVC-9904] override login page to set single sign-on text

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -92,6 +92,7 @@ def attempt_login(context, password):
 def login_link_visible(context):
     context.execute_steps(u"""
         Then I should see an element with xpath "//h1[contains(string(), 'Login')]"
+        And I should see "Sign in with your Digital ID"
     """)
 
 
