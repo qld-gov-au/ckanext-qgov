0cc90849f5a2b17581a0f9420f7483056f5ebdeb
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2026-08-24 12:11:37 +1000

    [QOLSVC-15761] update scenario test expectation to match new login page text

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -98,8 +98,8 @@ def attempt_login(context, password):
 def login_link_visible(context):
     context.execute_steps(u"""
         Then I should see an element with xpath "//h1[contains(string(), 'Login')]"
-        And I should see "Sign in with your Digital ID"
-        And I should see "Register via your Digital ID"
+        And I should see "Log in with single sign-on"
+        And I should see "Register with Digital ID"
     """)
 
 
